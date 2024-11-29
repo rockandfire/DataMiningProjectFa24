dec7cfb35f917a7e0f62952e0c09d2ccd5dd64bf
Rockandfire <jackson.wman@gmail.com>
2024-11-28 23:05:39 -0600

    support_3 needs work

--- monolith.py
+++ monolith.py
@@ -34,7 +34,7 @@ def extract_text_perms(card):
 	# For each rule, split each word and find all combinations of rules text in order.
 	# 
 	rule_phrases = []
-	rule_components = rules.strip().replace(',', '').replace('.', '').replace(':', '').split(' ')
+	rule_components = rules.lower().strip().replace(',', '').replace('.', '').replace(':', '').split(' ')
 
 	# For each word in parsed phrase, join next j words until end of phrase
 	for i in range(len(rule_components)):
@@ -44,7 +44,8 @@ def extract_text_perms(card):
 	return rule_phrases
 
 def get_similarity(card_a, card_b):
-	return len(set(card_a).intersection(set(card_b))) / (len(card_a) * len(card_b))
+	# print(set(rule_phrases[card_a]))
+	return len(set(rule_phrases[card_a]).intersection(set(rule_phrases[card_b]))) / len(rule_phrases[card_b])
 
 # Perform K-Means clustering on the set of valid 
 # cards using EDHRec Rank to determine distance
@@ -94,7 +95,7 @@ def k_means(k, cards):
 # on EDHRec rank. Starting with the most competitive cards, 
 # perform modified Apriori until the threshold of 63 cards
 # is met. The remaining 37 cards will be basic lands
-def apriori(commander_name, clusters, cards):
+def apriori(commander_name, clusters, cards):    
 
 	# Iterate over each cluster until length requirement is met
 	# When out of unique cards in cluster, move to next
@@ -117,7 +118,7 @@ def apriori(commander_name, clusters, cards):
 			for index in clusters[cluster_pos].index:
 
 				# get similarity between current card and each card already in deck
-				card_similarity = get_similarity(cards.loc[card], cards.loc[index])
+				card_similarity = get_similarity(card, index)
 				if card_similarity > max_similarity and index not in current_deck:
 					# print(card_similarity)
 					max_similarity = card_similarity 
@@ -139,40 +140,66 @@ def apriori(commander_name, clusters, cards):
 			cluster_pos += 1
 	# print(max(candidate_cards, key=lambda item: item[0]), cards.loc[max(candidate_cards, key=lambda item: item[0])[1]])
 
-	# print(cards.loc[max_index_a], cards.loc[max_index_b])
-	# print(len(current_deck))
-	# print(current_deck)
-	# print(similarities)
-	# print(cards[cards.index.isin(current_deck)][['name', 'edhrecRank']])
 	rules = []
 	for i in range(len(current_deck)):
 		rules.append((sim_card[i], current_deck[i], similarities[i], cards.loc[current_deck[i]]['edhrecRank']))
 
-	rules = sorted(rules, key=lambda x:x[3])
+	rules_2 = sorted(rules, key=lambda x:x[3])
 
-	return rules 
+	# Using rules of size 2, generate rules of size 3 and calculate support (similarity) and confidence
+	
+	rules = []
+	for card in current_deck:
+		max_similarity = -1
+		max_index_a = sys.maxsize
+		max_index_b = sys.maxsize
+		for rule in rules_2:	
+			# Get max similarity of card to either card in rule
+			card_similarity = get_similarity(rule[1], card)
+			if card_similarity > max_similarity:
+				# print(card_similarity)
+				max_similarity = card_similarity 
+				max_index_a = rule[0]
+				max_index_b = rule[1]
+				rule_2_similarity = rule[2]
+		# Find denom for support by taking average similarity for included over entire deck
+		sum = 0
+		for included_card in current_deck:
+			sum += get_similarity(included_card, card)
+		denom = sum / len(current_deck)
+
+		# Take sum of rule similarity and max / denom
+		support_3 = (card_similarity + rule_2_similarity) / denom
+
+		# Take sum of rule similarity and max / similarity to compute confidence for 3
+		confidence = (card_similarity + rule_2_similarity) / rule_2_similarity
+		rules.append((rule[0], rule[1], card, support_3, confidence, cards.loc[card]['edhrecRank']))
+
+	rules_3 = sorted(rules, key=lambda x:x[5])
+
+	return rules_2, rules_3
 
 class SimpleLinearRegression:
-    def __init__(self):
-        #initializes variables
-        self.slope_ = None
-        self.intercept_ = None
-        
-    def fit(self, x, y):
-        #calculates the mean of the input and labels
-        Xmean = np.mean(x)
-        ymean = np.mean(y)
-        
-        #calculate terms needed for slope and intercept of regression line
-        numerator = np.sum((x - Xmean) * (y - ymean))
-        denominator = np.sum((x - Xmean) ** 2)
+	def __init__(self):
+		#initializes variables
+		self.slope_ = None
+		self.intercept_ = None
+		
+	def fit(self, x, y):
+		#calculates the mean of the input and labels
+		Xmean = np.mean(x)
+		ymean = np.mean(y)
+		
+		#calculate terms needed for slope and intercept of regression line
+		numerator = np.sum((x - Xmean) * (y - ymean))
+		denominator = np.sum((x - Xmean) ** 2)
 
-        #calculate slope and intercept of regression line
-        self.slope_ = numerator / denominator
-        self.intercept_ = ymean - self.slope_ * Xmean
-        
-    def predict(self, x):
-        return self.intercept_ + self.slope_ * x
+		#calculate slope and intercept of regression line
+		self.slope_ = numerator / denominator
+		self.intercept_ = ymean - self.slope_ * Xmean
+		
+	def predict(self, x):
+		return self.intercept_ + self.slope_ * x
 	
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -205,43 +232,43 @@ preds = model.predict(Xval)
 #performs K fold cross validation on linear regression model
 def k_fold_cross_validation(model, Xtrain, ytrain, k):
 
-    #convert to numpy array for np functions
-    Xtrain = np.array(Xtrain)
-    ytrain = np.array(ytrain)
-    
-    #shuffle data
-    indices = np.arange(Xtrain.shape[0])
-    np.random.shuffle(indices)
-    Xtrain, ytrain = Xtrain[indices], ytrain[indices]
-    
-    #split data into folds
-    fold_size = len(Xtrain) // k
-    accuracies = []
-    
-    for i in range(k):
-        #split ddata into val and training sets
-        start, end = i * fold_size, (i + 1) * fold_size
-        X_val_fold = Xtrain[start:end]
-        y_val_fold = ytrain[start:end]
-        
-        X_train_fold = np.concatenate([Xtrain[:start], Xtrain[end:]])
-        y_train_fold = np.concatenate([ytrain[:start], ytrain[end:]])
-        
-        #fit model with selected data
-        model.fit(X_train_fold, y_train_fold)
-        
-        #eval current model
-        predictions = model.predict(X_val_fold)
-        #calculate mse
-        mse = np.mean((predictions - y_val_fold) ** 2)
-        accuracies.append(mse)
-    
-    mean_accuracy = np.mean(accuracies)
-    return mean_accuracy
+	#convert to numpy array for np functions
+	Xtrain = np.array(Xtrain)
+	ytrain = np.array(ytrain)
+	
+	#shuffle data
+	indices = np.arange(Xtrain.shape[0])
+	np.random.shuffle(indices)
+	Xtrain, ytrain = Xtrain[indices], ytrain[indices]
+	
+	#split data into folds
+	fold_size = len(Xtrain) // k
+	accuracies = []
+	
+	for i in range(k):
+		#split ddata into val and training sets
+		start, end = i * fold_size, (i + 1) * fold_size
+		X_val_fold = Xtrain[start:end]
+		y_val_fold = ytrain[start:end]
+		
+		X_train_fold = np.concatenate([Xtrain[:start], Xtrain[end:]])
+		y_train_fold = np.concatenate([ytrain[:start], ytrain[end:]])
+		
+		#fit model with selected data
+		model.fit(X_train_fold, y_train_fold)
+		
+		#eval current model
+		predictions = model.predict(X_val_fold)
+		#calculate mse
+		mse = np.mean((predictions - y_val_fold) ** 2)
+		accuracies.append(mse)
+	
+	mean_accuracy = np.mean(accuracies)
+	return mean_accuracy
 
 #prints predicted values versus actual values
 for x in range(5):
-    print(f'predicted: {preds.iloc[x, 0]}, actual: {yval.iloc[x, 0]}')
+	print(f'predicted: {preds.iloc[x, 0]}, actual: {yval.iloc[x, 0]}')
 
 print("Total card count for commander legality: {}".format(len(cards.index)))
 
@@ -258,24 +285,28 @@ cards = filter_identity(commander_name, cards)
 print("Total number of cards legal for deck: {}".format(len(cards.index)))
 
 # Extract all rules phrases for apriori comparison
-rule_phrases = []
+rule_phrases = {}
 for i in cards.index:
-	rule_phrases.append(extract_text_perms(cards.loc[i]))
+	rule_phrases[i] = extract_text_perms(cards.loc[i])
 
 # Perform K-Means clustering for k clusters on cards
-clusters = k_means(10, cards)
+clusters = k_means(12, cards)
 
 # Print length of each cluster
 for i in range(len(clusters)):
 	print("Number of cards in cluster {}: {}".format(i, len(clusters[i])))
 
 # Generate rules of length 2 for deck
-rules = apriori(commander_name, clusters, cards)
+rules_2, rules_3 = apriori(commander_name, clusters, cards)
 
 # Print each rule
-# for rule in rules:
+# for rule in rules_2:
 # 	print("Most similar card: {}\nAdded card: {}\nSimilarity Score: {}\nEDHRec Rank: {}\n".
 # 	format(cards.loc[rule[0]]['name'], cards.loc[rule[1]]['name'], rule[2], rule[3]))
+
+# for rule in rules_3:
+# 	print("Card 1: {}\nCard 2: {}\nCard 3: {}\nSimilarity Score: {}\nConfidence Score: {}\nEDHRec Rank: {}\n".
+# 	format(cards.loc[rule[0]]['name'], cards.loc[rule[1]]['name'], cards.loc[rule[2]]['name'], rule[3], rule[4], rule[5]))
 
 model = SimpleLinearRegression()
 mean_mse = k_fold_cross_validation(model, Xtrain, ytrain, 5)
